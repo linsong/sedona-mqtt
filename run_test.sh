983dc3f8bb1cdbfbd03614eb68ab22a70f19f459
Vincent Wang <vincent@linsong.org>
2018-06-16 16:29:07 +0800

    another try

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-(source adm/unix/init.sh && makeunixvm && makesedonac && sedonac scode/x86-test.xml && svm scode/x86-test.scode -utest communityMQTT)
+(source adm/unix/init.sh && python adm/unix/makeunixvm.py && python adm/makesedonac.py && ./bin/sedonac.sh scode/x86-test.xml && ./bin/svm scode/x86-test.scode -utest communityMQTT)
