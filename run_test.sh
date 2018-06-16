ce0bb56777a408c521d51be15acbf13c8f592c7c
Vincent Wang <vincent@linsong.org>
2018-06-16 16:24:46 +0800

    try to fix test

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-source adm/unix/init.sh 
-makeunixvm && makesedonac && sedonac scode/x86-test.xml && svm scode/x86-test.scode -utest communityMQTT
+(source adm/unix/init.sh && makeunixvm && makesedonac && sedonac scode/x86-test.xml && svm scode/x86-test.scode -utest communityMQTT)
