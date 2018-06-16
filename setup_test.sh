562bc7826633353ad7078058fa0799c58b336ba3
Vincent Wang <vincent@linsong.org>
2018-06-16 14:30:15 +0800

    first try to setup CI

---
+++ setup_test.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+# 
+# this script will patch sedona environment to support test communityMQTT kit 
+# 
+
+echo "patching platforms/src/generic/unix/generic-unix.xml ..."
+sed -i'' -e '\#<nativeKit depend="platUnix 1.0+" />#a \
+    <nativeKit depend="communityMQTT 1.0+" /> \
+' platforms/src/generic/unix/generic-unix.xml
+sed -i'' -e '\#<nativeSource path="/src/kits/datetimeStd/native/std"#a \
+    <nativeSource path="/src/kits/communityMQTT/native" /> \
+' platforms/src/generic/unix/generic-unix.xml
+
+echo "patching scode/x86-test.xml ..."
+sed -i'' -e '\#<depend on="types 1.2"   />#a \
+  <depend on="communityMQTT 1.2" /> \
+  ' scode/x86-test.xml
+
+echo "patching src/kits/dir.xml ..."
+sed -i'' -e '\#<target name="logManager" />#a \
+  <target name="communityMQTT" /> \
+  ' src/kits/dir.xml
+
